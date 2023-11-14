fb9c6ade05bd4e4bbe8420ed6d4faf3fe6a114bc
UmbertoSerino <serumberto@gmail.com>
2023-11-14 17:19:17 +0100

    created folder with html, css e script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+bisogna stampare in console dei numeri da 1 a 100:
+
+per multipli di 3 stampa Fizz, quindi se divisibile per 3
+
+per multipli di 5 stampa Buzz, quindi se divisibile per 5
+
+per multipli di 3 e 5 stampa fizzBuzz, quindi se divisibile per 3 e 5
+
+
+
+
