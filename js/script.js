7b2b5385872e28ef64979059692de1f6840fd76e
UmbertoSerino <serumberto@gmail.com>
2023-11-14 17:41:13 +0100

    added element in cycle

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
-bisogna stampare in console dei numeri da 1 a 100:
+// bisogna stampare in console dei numeri da 1 a 100:
 
-per multipli di 3 stampa Fizz, quindi se divisibile per 3
+// per multipli di 3 stampa Fizz, quindi se divisibile per 3
 
-per multipli di 5 stampa Buzz, quindi se divisibile per 5
+// per multipli di 5 stampa Buzz, quindi se divisibile per 5
 
-per multipli di 3 e 5 stampa fizzBuzz, quindi se divisibile per 3 e 5
+// per multipli di 3 e 5 stampa fizzBuzz, quindi se divisibile per 3 e 5
 
 
+// bisogna creare un ciclo con una condizione
 
+for (let i = 1; i <= 100; i++){
+
+    // se il numero è divisibile per 3
+    if (i % 3);
+    {
+    console.log(i);
+
+        // aggiungi Fizz
+
+        // se il numero è divisibile per 5
+    } if (i % 5);
+    {
+        // aggiungi buzz
+    }
+}
+
+                                                                                                                     
 
